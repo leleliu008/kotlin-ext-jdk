8e1e8f44167606480c5b7d010b66795194a492c0
leleliu008 <792793182@qq.com>
2019-09-12 16:25:01 +0800

    修改build.gradle.kts的配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,20 @@
 plugins {
+    //https://docs.gradle.org/current/userguide/java_plugin.html
     java
+
+    //https://docs.gradle.org/current/userguide/maven_plugin.html
     maven
+
+    //https://github.com/leleliu008/BintrayUploadGradlePlugin
+    //https://plugins.gradle.org/plugin/com.fpliu.bintray
+    id("com.fpliu.bintray").version("1.0.7")
 
     //用于上传到jCenter中
     //https://github.com/bintray/gradle-bintray-plugin
     id("com.jfrog.bintray").version("1.7.3")
+    
+    //https://kotlinlang.org/docs/reference/using-gradle.html
     kotlin("jvm") version "1.3.50"
-}
-
-buildscript {
-    repositories {
-        jcenter { url = uri("https://maven.aliyun.com/repository/jcenter") }
-    }
-    dependencies {
-        classpath("com.fpliu:BintrayUploadGradlePlugin:1.0.7")
-    }
-}
-
-apply {
-    plugin("com.fpliu.bintray")
 }
 
 sourceSets["main"].java.srcDir("src/main/kotlin")
@@ -30,7 +26,7 @@ version = "1.0.1"
 
 val projectName = project.name
 
-(project.extensions.getByName("bintrayUploadExtension") as com.fpliu.gradle.BintrayUploadExtension).apply {
+bintrayUploadExtension {
     developerName = "leleliu008"
     developerEmail = "leleliu008@gamil.com"
 
